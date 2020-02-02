e87083e11be126cc77bfee19173aacdef6b5ccd4
mohneeshdamade <mohneesh.damade@gmail.com>
2020-02-02 16:13:51 +0530

    analyze the output

---
+++ Main.java
@@ -0,0 +1,30 @@
+/**
+ * Predict the output of this code, by analyzing the memory changes in stack and heap
+ * @author Mohneesh
+ *
+ */
+
+public class Main {
+
+	public static void main(String args[]) {
+		Main main = new Main();
+		main.start();
+	}
+	
+	public void start() {
+		String last = "Z";
+		Container container = new Container();
+		container.setInitial("C");
+		another(container,last);
+		System.out.print(container.getInitial());
+	}
+	
+	public void another(Container initialHolder, String newInitial) {
+		newInitial.toLowerCase();
+		initialHolder.setInitial("B");
+		Container initial2 = new Container();
+		initialHolder=initial2;
+		System.out.print(initialHolder.getInitial());
+		System.out.print(newInitial);
+	}
+}
